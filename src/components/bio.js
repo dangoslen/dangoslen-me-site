743f61767f073ec00fc9c42b5d6bcf430f223b90
Dan Goslen <dwgoslen@gmail.com>
2021-08-30 16:30:47 -0400

    Adding refferral link (#62)

--- src/components/bio.js
+++ src/components/bio.js
@@ -5,67 +5,68 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
-import styled from "styled-components"
-
-import { rhythm } from "../utils/typography"
-
-function Bio() {
-  return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author } = data.site.siteMetadata
-        return (
-          <Container>
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            />
-            <p>
-            Dan Goslen is a software engineer and sustainable development advocate. He has spent 10 years writing quality software systems that range
-            from monoliths to micro-services and everywhere in between. He currently works { <a href="https://bandwidth.com">@Bandwidth</a> } in Raleigh, NC where he lives with his wife.
-            </p>
-          </Container>
-        )
-      }}
-    />
-  )
-}
-
-const bioQuery = graphql`
-  query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-    site {
-      siteMetadata {
-        author
-        social {
-          twitter
-        }
-      }
-    }
-  }
-`
-
-const Container = styled.div`
-  display: flex;
-`
-
-export default Bio
+ import React from "react"
+ import { StaticQuery, graphql } from "gatsby"
+ import Image from "gatsby-image"
+ import styled from "styled-components"
+ 
+ import { rhythm } from "../utils/typography"
+ 
+ function Bio() {
+   return (
+     <StaticQuery
+       query={bioQuery}
+       render={data => {
+         const { author } = data.site.siteMetadata
+         return (
+           <Container>
+             <Image
+               fixed={data.avatar.childImageSharp.fixed}
+               alt={author}
+               style={{
+                 marginRight: rhythm(1 / 2),
+                 marginBottom: 0,
+                 minWidth: 50,
+                 borderRadius: `100%`,
+               }}
+               imgStyle={{
+                 borderRadius: `50%`,
+               }}
+             />
+             <p>
+             Dan Goslen is a software engineer and sustainable development advocate. He has spent 10 years writing quality software systems that range
+             from monoliths to micro-services and everywhere in between. He currently works { <a href="https://bandwidth.com">@Bandwidth</a> } (and we are <a href="https://grnh.se/rvdtbj8b1">hiring right now!</a>) in Raleigh, NC where he lives with his wife.
+             </p>
+           </Container>
+         )
+       }}
+     />
+   )
+ }
+ 
+ const bioQuery = graphql`
+   query BioQuery {
+     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+       childImageSharp {
+         fixed(width: 50, height: 50) {
+           ...GatsbyImageSharpFixed
+         }
+       }
+     }
+     site {
+       siteMetadata {
+         author
+         social {
+           twitter
+         }
+       }
+     }
+   }
+ `
+ 
+ const Container = styled.div`
+   display: flex;
+ `
+ 
+ export default Bio
+ 
