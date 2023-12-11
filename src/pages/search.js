6c652125bc98a6e4ff84fce0fac6cccc39200753
Dan Goslen <dwgoslen@gmail.com>
2023-12-10 21:24:07 -0500

    Basic search working

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,35 +1,87 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React, { useState, useMemo } from 'react'
+import { useFlexSearch } from 'react-use-flexsearch'
+import { graphql } from "gatsby"
+import styled from 'styled-components'
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BlogSummary from "../components/blog-summary"
 
-class Search extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = "Blog"
-    const posts = data.posts.edges
+const mappedDefault = store => {
+  return Object.values(store)
+}
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All Posts" description="Dan Goslen's blog on software engineering" />
-      
-        <div style={{ margin: "20px 0 40px" }}>
-          { posts.map(({ node }) => {
-              return ( <BlogSummary node={ node } key={node.fields.slug} /> )
-          })}
+const Search = props => {
+  const { data } = props
+  const { site, localSearchPosts } = data
+  const { siteTitle } = site.siteMetadata
+  const { index, store } = localSearchPosts
+
+  const params = new URLSearchParams(props.location.search);
+  const searchQuery = params.get('search')
+
+  const allPosts = useMemo(() => mappedDefault(store), [store]);
+  const [query, setQuery] = useState(searchQuery || '', null)
+  const [timer, setTimer] = useState(null, null)
+  let results = useFlexSearch(query, index, store)
+  console.log(query)
+  if (query === '') {
+    results = allPosts
+  }
+
+  const handleChange = event => {
+    if (timer != null) {
+      clearTimeout(timer)
+    }
+    
+    const newQuery = event.target.value
+    const newTimer = setTimeout(() => setQuery(newQuery), newQuery === '' ? 100 : 600)
+    setTimer(newTimer)
+  }
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO title="Dan Goslen Blog" description="Dan Goslen's blog on software engineering" />
+
+      <form>
+        <SearchBar
+          type='text' 
+          placeholder='Search a word or phrase or tag...'
+          defaultValue={ searchQuery || '' }
+          onChange={ handleChange } />
+      </form>
+
+      <div style={{ margin: "20px 0 40px" }}>
+        { results.map(( node ) => {
+            return ( <BlogSummary node={ node } key={ node.slug } /> )
+        })}
+
+        <div style={{ marginTop: "40px" }}>
+
+          <hr/>
 
           <Bio />
 
         </div>
-      </Layout>
-    )
-  }
+
+      </div>
+    </Layout>
+  )
 }
 
 export default Search
+
+const SearchBar = styled.input`
+  margin-top: 10px;
+  margin-bottom: -20px;
+  width: 100%;
+  border-radius: 30px;
+  padding-left: 12px;
+  padding-top: 3px;
+  padding-bottom: 3px;
+  border-color: #d0b6b08a;
+`
 
 export const pageQuery = graphql`
   query {
